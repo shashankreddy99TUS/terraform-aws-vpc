aefa9b1d3bafe43718b638b137960b55bd9aeaa6
shashankreddy99TUS <k.shashankreddy599@gmail.com>
2025-07-03 18:57:28 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -8,18 +8,18 @@
 #     value = aws_subnet.public
 # }
 
-# output "vpc_id" {
-#   value = aws_vpc.main.id
-# }
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 
-# output "public_subnet_ids" {
-#   value = aws_subnet.public[*].id
-# }
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
 
-# output "private_subnet_ids" {
-#   value = aws_subnet.private[*].id
-# }
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
 
-# output "database_subnet_ids" {
-#   value = aws_subnet.database[*].id
-# }
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
